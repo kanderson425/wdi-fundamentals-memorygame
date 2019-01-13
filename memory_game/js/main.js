bc0535ea9bbada69b20ed2928494a15a5189cc1d
Kyle Anderson <kyleanderson@Kyles-MacBook-Pro.local>
2019-01-13 08:46:52 -0600

    created some logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
 console.log("Up and running!");
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: 'images/queen-of-hearts.png'
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: 'images/queen-of-diamonds.png'
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: 'images/king-of-hearts.png'
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: 'images/king-of-diamonds.png'
+}
+];
 
 var cardsInPlay = [];
 
@@ -14,23 +35,28 @@ console.log("Sorry, try again.");
 }
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 
 	checkForMatch;
 }
+
  	flipCard;
 
 	flipCard(0);
+	console.log(cards[0].cardImage);
+	console.log(cards[0].suit);
 
 	flipCard(2);
+	console.log(cards[2].cardImage);
+	console.log(cards[2].suit);
 
 if(cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else alert("Sorry, try again!");
 }
+
 
 
 
